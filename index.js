7b3d1a4064db385f735a21968629cdb09f4faab2
Ben <bennaat@gmail.com>
2024-05-13 23:52:02 +0700

    exercise 4 control flow

---
+++ index.js
@@ -0,0 +1,10 @@
+let dayNumber = 112;
+
+dayNumber === 0 ? console.log("Sunday") :
+dayNumber === 1 ? console.log("Monday") :
+dayNumber === 2 ? console.log("Tuesday") :
+dayNumber === 3 ? console.log("Wednesday") :
+dayNumber === 4 ? console.log("Thursday") :
+dayNumber === 5 ? console.log("Friday") :
+dayNumber === 6 ? console.log("Saturday") :
+console.log("Invalid day number!!!!");
